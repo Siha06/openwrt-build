0b68af1c3497360b7e802a6b39696abb92b6e9c9
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-03-03 16:11:10 +0800

    Update 199-rockchip.sh

--- patch/openwrt-24.10/199-rockchip.sh
+++ patch/openwrt-24.10/199-rockchip.sh
@@ -8,9 +8,9 @@ uci delete ttyd.@ttyd[0].interface
 uci set dropbear.@dropbear[0].Interface=''
 
 #其他网络设置
-uci del wireless.radio0.disabled
-uci del wireless.default_radio0.disabled
-uci commit wireless
+#uci del wireless.radio0.disabled
+#uci del wireless.default_radio0.disabled
+#uci commit wireless
 
 #uci set network.usbwan=interface
 #uci set network.usbwan.proto='dhcp'
@@ -24,7 +24,7 @@ sed -i 's#downloads.openwrt.org#mirrors.pku.edu.cn/openwrt#g' /etc/opkg/distfeed
 sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/openwrt/releases/24.10.0/targets/rockchip/armv8/kmods/6.6.73-1-f35e93bc2c89b98d107e57cdea041972' /etc/opkg/customfeeds.conf
 sed -i '$a src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/aarch64_generic/kiddin9' /etc/opkg/customfeeds.conf
 
-wifi up
+#wifi up
 /etc/init.d/network restart
 
 exit 0
