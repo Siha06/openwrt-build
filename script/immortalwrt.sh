a7794c24c3923fc44ee559ac5d8295241807e114
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-03-25 20:42:18 +0800

    Update immortalwrt.sh

--- script/immortalwrt.sh
+++ script/immortalwrt.sh
@@ -5,7 +5,7 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/l
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 #sed -i 's/ImmortalWrt/WiFi/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 #sed -i 's/ImmortalWrt/WiFi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-mv $GITHUB_WORKSPACE/patch/imm-24.10/199-rockchip.sh package/base-files/files/etc/uci-defaults/199-rockchip.sh
+mv $GITHUB_WORKSPACE/patch/imm23.05/199-rockchip.sh package/base-files/files/etc/uci-defaults/199-rockchip.sh
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
 
 git clone --depth 1 -b core https://github.com/vernesong/OpenClash.git  package/openclash-core
