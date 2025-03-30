f8a55c8897f26a6e4711b61c1195dbef6123ed10
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-03-30 20:54:00 +0800

    Update 998-ipq60xx.sh

--- patch/ipq-vikingyfy/998-ipq60xx.sh
+++ patch/ipq-vikingyfy/998-ipq60xx.sh
@@ -3,8 +3,9 @@
 sed -i '/nss/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
 sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/apk/repositories.d/distfeeds.list
-sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.6.77-1-51630055abc52e4640365e0d2a28a72e/packages.adb' /etc/apk/repositories.d/customfeeds.list
-sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.6.83-1-6466bd46a803e6572a94d1ed46a0536e/packages.adb' /etc/apk/repositories.d/customfeeds.list
+#sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.6.77-1-51630055abc52e4640365e0d2a28a72e/packages.adb' /etc/apk/repositories.d/customfeeds.list
+sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.6.83-1-ea69bec1cc40cedb58b57bb61fc65eab/packages.adb' /etc/apk/repositories.d/customfeeds.list
+
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
 
