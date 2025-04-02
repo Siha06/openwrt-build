8dd1813dc3c641e80b1a69379bb68a7cf8800ef5
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-04-02 14:03:34 +0800

    Update padavanonly-7915.sh

--- script/padavanonly-7915.sh
+++ script/padavanonly-7915.sh
@@ -2,10 +2,17 @@ sed -i 's/192.168.6.1/192.168.23.1/g' package/base-files/files/bin/config_genera
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 mv $GITHUB_WORKSPACE/patch/7621-237imm/999-diy package/base-files/files/etc/uci-defaults/zz-diy
+
+if grep -q "openclash=y" "$GITHUB_WORKSPACE/$CONFIG_FILE"; then
+    git clone --depth 1 -b core https://github.com/vernesong/OpenClash.git  package/openclash-core
+    mv package/openclash-core/master/meta/clash-linux-mipsle-softfloat.tar.gz package/base-files/files/etc/clash-linux-mipsle-softfloat.tar.gz
+    rm -rf package/openclash-core
+fi
+
 #mv $GITHUB_WORKSPACE/patch/7621-237imm/adg.tar.gz package/base-files/files/etc/adg.tar.gz
-mv $GITHUB_WORKSPACE/patch/7621-237imm/adg/AdGuardHome.yaml package/base-files/files/etc/my-adgh.yaml
-mv $GITHUB_WORKSPACE/patch/7621-237imm/adg/firewall package/base-files/files/etc/my-firewall
-mv $GITHUB_WORKSPACE/patch/7621-237imm/adg/firewall.user package/base-files/files/etc/my-firewall.user
+#mv $GITHUB_WORKSPACE/patch/7621-237imm/adg/AdGuardHome.yaml package/base-files/files/etc/my-adgh.yaml
+#mv $GITHUB_WORKSPACE/patch/7621-237imm/adg/firewall package/base-files/files/etc/my-firewall
+#mv $GITHUB_WORKSPACE/patch/7621-237imm/adg/firewall.user package/base-files/files/etc/my-firewall.user
 
 #rm -rf feeds/luci/applications/luci-app-filetransfer
 rm -rf package/emortal/luci-app-mwan3helper-chinaroute
@@ -16,7 +23,7 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
 rm -rf feeds/packages/lang/golang
-#git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 #删除自带的老旧依赖，ssr-plus，passwall
 rm -rf feeds/packages/net/{brook,chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,microsocks,naiveproxy}
 rm -rf feeds/packages/net/{pdnsd-alt,simple-obfs,sing-box,tcping,trojan*,tuic-client,v2ray*,xray*,mosdns,redsocks2}
