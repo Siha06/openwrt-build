3e14815d6fecda08daf9177774684414fe7745e3
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-04-03 14:11:39 +0800

    Update 199-arm64.sh

--- patch/openwrt-24.10/199-arm64.sh
+++ patch/openwrt-24.10/199-arm64.sh
@@ -31,9 +31,7 @@ uci delete ttyd.@ttyd[0].interface
 uci set dropbear.@dropbear[0].Interface=''
 
 #其他网络设置
-uci del wireless.radio0.disabled
-uci del wireless.default_radio0.disabled
-uci commit wireless
+
 uci set network.lan.ip6assign='64'
 uci set network.lan.ip6ifaceid='eui64'
 uci commit network
@@ -44,7 +42,6 @@ sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.openwrt.org#mirrors.pku.edu.cn/openwrt#g' /etc/opkg/distfeeds.conf
 sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/openwrt/releases/24.10.0/targets/armsr/armv8/kmods/6.6.73-1-5f0e657555378f3e0673e256ddb6fb86' /etc/opkg/customfeeds.conf
 
-wifi up
-/etc/init.d/network restart
+#/etc/init.d/network restart
 
 exit 0
