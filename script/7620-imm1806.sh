d5fc16a78e63aeca6c6ecc36bba4f9b299762c32
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-04-09 01:33:13 +0800

    Update 7620-imm1806.sh

--- script/7620-imm1806.sh
+++ script/7620-imm1806.sh
@@ -2,7 +2,8 @@ sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_genera
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-mv $GITHUB_WORKSPACE/patch/7621-237imm/999-diy package/base-files/files/etc/uci-defaults/zz-diy
+#mv $GITHUB_WORKSPACE/patch/7621-237imm/999-diy package/base-files/files/etc/uci-defaults/zz-diy
+mv $GITHUB_WORKSPACE/patch/imm21.02/199-mt762x package/base-files/files/etc/uci-defaults/zz-diy
 
 #rm -rf feeds/luci/modules/luci-base/po/zh-cn
 #rm -rf feeds/luci/applications/luci-app-passwall/po/zh-cn
