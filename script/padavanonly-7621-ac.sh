5f1dad52e38746f4a3cca1c5cbdeda535ab347d5
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-04-13 01:12:25 +0800

    Update padavanonly-7621-ac.sh

--- script/padavanonly-7621-ac.sh
+++ script/padavanonly-7621-ac.sh
@@ -18,9 +18,8 @@ git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git f
 rm -rf feeds/packages/lang/golang
 git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 #删除自带的老旧依赖，ssr-plus，passwall
-rm -rf feeds/packages/net/{brook,chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,microsocks,naiveproxy}
-rm -rf feeds/packages/net/{simple-obfs,sing-box,tcping,trojan*,tuic-client,v2ray*,xray*,mosdns,redsocks2}
-rm -rf feeds/packages/net/{ssocks,shadow-tls}
+rm -rf feeds/packages/net/{chinadns-ng,dns2socks,dns2tcp,dnsproxy,hysteria,ipt2socks,microsocks,naiveproxy}
+rm -rf feeds/packages/net/{simple-obfs,sing-box,tcping,trojan*,tuic-client,v2ray*,xray*,mosdns,redsocks2,shadow-tls}
 rm -rf feeds/packages/devel/gn
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ssr-plus,luci-app-mosdns,luci-app-openclash}
