d69edaf3c03f3b7ab4720e3a7a06f780f78d8208
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-04-23 02:39:46 +0800

    Update ARM64.sh

--- script/ARM64.sh
+++ script/ARM64.sh
@@ -9,9 +9,11 @@ mv $GITHUB_WORKSPACE/patch/imm-24.10/199-arm64.sh package/base-files/files/etc/u
 
 if grep -q "openclash=y" "$GITHUB_WORKSPACE/$CONFIG_FILE"; then
     git clone --depth 1 -b core https://github.com/vernesong/OpenClash.git  package/openclash-core
-    mv package/openclash-core/master/meta/clash-linux-arm64.tar.gz package/base-files/files/etc/clash-linux-arm64.tar.gz
+    tar -zxf package/openclash-core/master/meta/clash-linux-arm64.tar.gz -C package/base-files/files/etc/
+    mv package/base-files/files/etc/clash package/base-files/files/etc/my-clash
     rm -rf package/openclash-core
 fi
+
 
 #完全删除luci版本
 sed -i "s/+ ' \/ ' : '') + (luciversion ||/:/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
