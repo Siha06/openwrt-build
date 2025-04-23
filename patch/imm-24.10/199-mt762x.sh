df756e0933175ca0a0f8a05da2d7e21678c08004
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-04-24 03:24:18 +0800

    Update 199-mt762x.sh

--- patch/imm-24.10/199-mt762x.sh
+++ patch/imm-24.10/199-mt762x.sh
@@ -10,6 +10,7 @@ uci set dropbear.@dropbear[0].Interface=''
 
 uci commit
 
+sed -i '/modem/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
 sed -i '$a src/gz kmods https://mirror.nju.edu.cn/immortalwrt/releases/24.10.0/targets/ramips/mt7621/kmods/6.6.73-1-bb862acff14b3e53e1a47bb4219811fb' /etc/opkg/customfeeds.conf
