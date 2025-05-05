df0feada49f1a47ded277bdfcb0476801c501130
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-05-05 09:50:01 +0800

    Update 199-mydef.sh

--- patch/ipq-breeze/199-mydef.sh
+++ patch/ipq-breeze/199-mydef.sh
@@ -2,7 +2,7 @@
 
 
 sed -i '$a src/gz openwrt_others https://mirror.nju.edu.cn/immortalwrt/releases/24.10-SNAPSHOT/targets/qualcommax/ipq807x/packages' /etc/opkg/customfeeds.conf
-#sed -i '$a src/gz openwrt_kmods https://mirror.nju.edu.cn/immortalwrt/releases/24.10-SNAPSHOT/targets/qualcommax/ipq807x/kmods/6.6.65-1-c9824125bf4a8028af70bed3c9a6ae77' /etc/opkg/customfeeds.conf
+sed -i '$a #src/gz openwrt_kmods https://mirror.nju.edu.cn/immortalwrt/releases/24.10-SNAPSHOT/targets/qualcommax/ipq807x/kmods/6.6.65-1-c9824125bf4a8028af70bed3c9a6ae77' /etc/opkg/customfeeds.conf
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
