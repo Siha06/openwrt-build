41af0ed778bdaa5cb8f7940f80fcf5061a160b7d
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-05-14 02:45:50 +0800

    Update 998-ipq60xx.sh

--- patch/ipq-vikingyfy/998-ipq60xx.sh
+++ patch/ipq-vikingyfy/998-ipq60xx.sh
@@ -14,8 +14,9 @@ uci delete ttyd.@ttyd[0].interface
 uci set dropbear.@dropbear[0].Interface=''
 
 # wifi设置
-uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
-uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
+mymac=$(ifconfig br-lan 2>/dev/null | awk '/HWaddr/ {print $5}' | awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')
+uci set wireless.default_radio0.ssid=WiFi-${mymac}-5G
+uci set wireless.default_radio1.ssid=WiFi-${mymac}-2.4G
 uci commit wireless
 
 #uci set network.lan.ipaddr='192.168.1.1'
