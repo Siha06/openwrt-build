5446d5c455cffc6794777611d7775bafacf2dd22
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-05-31 12:07:43 +0800

    Update 998-ipq.sh

--- patch/ipq-vikingyfy/998-ipq.sh
+++ patch/ipq-vikingyfy/998-ipq.sh
@@ -18,9 +18,8 @@ uci set dropbear.@dropbear[0].Interface=''
 #uci commit luci
 
 # wifi设置
-mymac=$(cat /sys/class/net/br-lan/address 2>/dev/null | awk -F: '{print $5 $6}' | tr 'a-z' 'A-Z')
-uci set wireless.default_radio0.ssid=WiFi-${mymac}-5G
-uci set wireless.default_radio1.ssid=WiFi-${mymac}-2.4G
+uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
+uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
 uci commit wireless
 
 #uci set network.lan.ipaddr='192.168.1.1'
