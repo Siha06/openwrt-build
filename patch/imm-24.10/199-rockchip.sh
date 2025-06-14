3bcfa181ee438a404ac2f0e0fc82d60ed06a38ee
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-06-15 00:11:26 +0800

    Update 199-rockchip.sh

--- patch/imm-24.10/199-rockchip.sh
+++ patch/imm-24.10/199-rockchip.sh
@@ -14,8 +14,8 @@ uci commit
 sed -i '/modem/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.1/targets/rockchip/armv8/kmods/6.6.86-1-422144fea623288f7402e1a9a15724c8' /etc/opkg/customfeeds.conf
-#sed -i '$a src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/aarch64_generic/kiddin9' /etc/opkg/customfeeds.conf
+sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.1/targets/rockchip/armv8/kmods/6.6.86-1-422144fea623288f7402e1a9a15724c8' /etc/opkg/distfeeds.conf
+sed -i '$a #src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/aarch64_generic/kiddin9' /etc/opkg/customfeeds.conf
 
 #uci set network.usbwan=interface
 #uci set network.usbwan.proto='dhcp'
