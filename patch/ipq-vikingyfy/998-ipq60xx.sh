106175e8bdda380c77ef08094576a216039bf826
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-06-14 22:35:15 +0800

    Update 998-ipq60xx.sh

--- patch/ipq-vikingyfy/998-ipq60xx.sh
+++ patch/ipq-vikingyfy/998-ipq60xx.sh
@@ -4,8 +4,8 @@ sed -i '/nss/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/qualcommax/d' /etc/apk/repositories.d/distfeeds.list
 sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/apk/repositories.d/distfeeds.list
-sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.12.30-1-601467766086dd11d1129a2de6906a99/packages.adb' /etc/apk/repositories.d/customfeeds.list
-sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/packages/packages.adb' /etc/apk/repositories.d/customfeeds.list
+sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.12.33-1-5d212070dc1bc064bea33e9afb84f20f/packages.adb' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/packages/packages.adb' /etc/apk/repositories.d/distfeeds.list
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
@@ -29,10 +29,8 @@ uci commit wireless
 #uci commit network
 uci commit
 
-OPENCLASH_FILE="/etc/config/openclash"
-if [ -f "$OPENCLASH_FILE" ]; then
-    mv /etc/my-clash /etc/openclash/core/clash_meta
-fi
+
+
 /etc/init.d/network restart
 
 exit 0
