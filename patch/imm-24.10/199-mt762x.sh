7958de52d53632c4c268bc44b468baceaf54da04
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-06-21 13:47:28 +0800

    Update 199-mt762x.sh

--- patch/imm-24.10/199-mt762x.sh
+++ patch/imm-24.10/199-mt762x.sh
@@ -13,7 +13,7 @@ uci commit
 sed -i '/modem/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz kmods https://mirror.nju.edu.cn/immortalwrt/releases/24.10.0/targets/ramips/mt7621/kmods/6.6.73-1-bb862acff14b3e53e1a47bb4219811fb' /etc/opkg/customfeeds.conf
+sed -i '$a src/gz kmods https://mirror.nju.edu.cn/immortalwrt/releases/24.10.0/targets/ramips/mt7621/kmods/6.6.86-1-1e9929ab745a88954c978e6f878f4f58' /etc/opkg/customfeeds.conf
 #sed -i '$a src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/aarch64_generic/kiddin9' /etc/opkg/customfeeds.conf
 
 OPENCLASH_FILE="/etc/config/openclash"
