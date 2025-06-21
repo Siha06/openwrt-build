28e6e7f0d61d1af7b1b6e7a3c241a91a0a625d11
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-06-21 13:54:20 +0800

    Update 199-mt762x.sh

--- patch/imm-24.10/199-mt762x.sh
+++ patch/imm-24.10/199-mt762x.sh
@@ -16,6 +16,12 @@ sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/
 sed -i '$a src/gz kmods https://mirror.nju.edu.cn/immortalwrt/releases/24.10.0/targets/ramips/mt7621/kmods/6.6.86-1-1e9929ab745a88954c978e6f878f4f58' /etc/opkg/customfeeds.conf
 #sed -i '$a src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/aarch64_generic/kiddin9' /etc/opkg/customfeeds.conf
 
+uci add firewall rule
+uci set firewall.@rule[-1].src='wan'
+uci set firewall.@rule[-1].name='allow5422'
+uci set firewall.@rule[-1].dest_port='5422'
+uci set firewall.@rule[-1].target='ACCEPT'
+
 OPENCLASH_FILE="/etc/config/openclash"
 if [ -f "$OPENCLASH_FILE" ]; then
     mv /etc/my-clash /etc/openclash/core/clash_meta
