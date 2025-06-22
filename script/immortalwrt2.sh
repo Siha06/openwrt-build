88fb2a94c8acc03bacc79bb7d79690c498bd6f93
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-06-22 15:56:24 +0800

    Update immortalwrt2.sh

--- script/immortalwrt2.sh
+++ script/immortalwrt2.sh
@@ -1,4 +1,4 @@
-sed -i 's/192.168.1.1/192.168.101.1g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.101.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/WiFi/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
