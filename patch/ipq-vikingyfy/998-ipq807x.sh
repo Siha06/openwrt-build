fcbc7e1a06b1635726c59a75dff58719e2c84e70
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-07-04 06:32:59 +0800

    Update 998-ipq807x.sh

--- patch/ipq-vikingyfy/998-ipq807x.sh
+++ patch/ipq-vikingyfy/998-ipq807x.sh
@@ -4,7 +4,7 @@ sed -i '/nss/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/qualcommax/d' /etc/apk/repositories.d/distfeeds.list
 sed -i 's#https://downloads.immortalwrt.org#http://mirrors.pku.edu.cn/immortalwrt#g' /etc/apk/repositories.d/distfeeds.list
-sed -i '$a http://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq807x/kmods/6.6.85-1-46b4d6b592658cfd4beedf8fd149c294/packages.adb' /etc/apk/repositories.d/customfeeds.list
+sed -i '$a http://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq807x/kmods/6.12.35-1-e7bb587fd1070df0ec93f514bf1e0f0f/packages.adb' /etc/apk/repositories.d/customfeeds.list
 sed -i '$a http://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq807x/packages/packages.adb' /etc/apk/repositories.d/customfeeds.list
 
 # 设置所有网口可访问网页终端
@@ -17,15 +17,10 @@ uci set dropbear.@dropbear[0].Interface=''
 #uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
 #uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
 
-uci set network.lan.netmask='255.0.0.0'
-uci commit network
 uci commit
 
-OPENCLASH_FILE="/etc/config/openclash"
-if [ -f "$OPENCLASH_FILE" ]; then
-    mv /etc/my-clash /etc/openclash/core/clash_meta
-fi
+cp /etc/my-clash /etc/openclash/core/clash_meta
 
-/etc/init.d/network restart
+# /etc/init.d/network restart
 
 exit 0
