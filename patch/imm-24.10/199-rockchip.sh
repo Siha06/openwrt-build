0560ecaed926285f50e9e782827033e2b36ab6e6
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-07-18 02:30:15 +0800

    Update 199-rockchip.sh

--- patch/imm-24.10/199-rockchip.sh
+++ patch/imm-24.10/199-rockchip.sh
@@ -14,20 +14,17 @@ uci commit
 sed -i '/modem/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.1/targets/rockchip/armv8/kmods/6.6.86-1-422144fea623288f7402e1a9a15724c8' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.2/targets/rockchip/armv8/kmods/6.6.93-1-9e686cc1e0d5129337ca1ca28c2ab984' /etc/opkg/distfeeds.conf
 sed -i '$a #src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/aarch64_generic/kiddin9' /etc/opkg/customfeeds.conf
 
-sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
-sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+#sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+#sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 
 #uci set network.usbwan=interface
 #uci set network.usbwan.proto='dhcp'
 #uci commit network
 
-OPENCLASH_FILE="/etc/config/openclash"
-if [ -f "$OPENCLASH_FILE" ]; then
-    mv /etc/my-clash /etc/openclash/core/clash_meta
-fi
+cp /etc/my-clash /etc/openclash/core/clash_meta
 
 #uci set network.cfg030f15.macaddr="8C:DA:$(date +"%d:%H:%M:%S")"
 #uci set network.cfg060f15.macaddr="9C:DA:$(date +"%d:%H:%M:%S")"
@@ -36,7 +33,7 @@ fi
 #uci commit wireless
 
 uci commit
-#mv /etc/my-crontabs /etc/crontabs/root
+
 #/etc/init.d/network restart
 
 exit 0
