54b86d973637fd4406183ad620f2aabc5b210f81
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-07-23 16:36:31 +0800

    Update 199-mt762x.sh

--- patch/imm-24.10/199-mt762x.sh
+++ patch/imm-24.10/199-mt762x.sh
@@ -16,6 +16,16 @@ sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/
 sed -i '$a src/gz kmods https://downloads.immortalwrt.org/releases/24.10.2/targets/ramips/mt7621/kmods/6.6.93-1-730c62a2d59c0c43f493ba6554a36be8' /etc/opkg/customfeeds.conf
 #sed -i '$a src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/aarch64_generic/kiddin9' /etc/opkg/customfeeds.conf
 
+# wifi设置
+#uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
+#uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
+uci set wireless.default_radio0.ssid=FuRui_Lan
+uci set wireless.default_radio1.ssid=FuRui_Lan
+uci set wireless.default_radio0.encryption=psk2+ccmp
+uci set wireless.default_radio1.encryption=psk2+ccmp
+uci set wireless.default_radio0.key=12345678 
+uci set wireless.default_radio1.key=12345678 
+
 #uci add firewall rule
 #uci set firewall.@rule[-1].src='wan'
 #uci set firewall.@rule[-1].name='allow5422'
