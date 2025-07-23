ac94385c2829157fcf2bc6772eb15f9a8556f77c
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-07-23 16:21:17 +0800

    Update 199-mt762x.sh

--- patch/imm-24.10/199-mt762x.sh
+++ patch/imm-24.10/199-mt762x.sh
@@ -13,21 +13,18 @@ uci commit
 sed -i '/modem/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz kmods https://mirror.nju.edu.cn/immortalwrt/releases/24.10.0/targets/ramips/mt7621/kmods/6.6.86-1-1e9929ab745a88954c978e6f878f4f58' /etc/opkg/customfeeds.conf
+sed -i '$a src/gz kmods https://downloads.immortalwrt.org/releases/24.10.2/targets/ramips/mt7621/kmods/6.6.93-1-730c62a2d59c0c43f493ba6554a36be8' /etc/opkg/customfeeds.conf
 #sed -i '$a src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/aarch64_generic/kiddin9' /etc/opkg/customfeeds.conf
 
-uci add firewall rule
-uci set firewall.@rule[-1].src='wan'
-uci set firewall.@rule[-1].name='allow5422'
-uci set firewall.@rule[-1].dest_port='5422'
-uci set firewall.@rule[-1].target='ACCEPT'
-uci commit firewall
-/etc/init.d/firewall restart >/dev/null 2>&1
+#uci add firewall rule
+#uci set firewall.@rule[-1].src='wan'
+#uci set firewall.@rule[-1].name='allow5422'
+#uci set firewall.@rule[-1].dest_port='5422'
+#uci set firewall.@rule[-1].target='ACCEPT'
+#uci commit firewall
+#/etc/init.d/firewall restart >/dev/null 2>&1
 
-OPENCLASH_FILE="/etc/config/openclash"
-if [ -f "$OPENCLASH_FILE" ]; then
-    mv /etc/my-clash /etc/openclash/core/clash_meta
-fi
+cp /etc/my-clash /etc/openclash/core/clash_meta
 
 #/etc/init.d/network restart
 
