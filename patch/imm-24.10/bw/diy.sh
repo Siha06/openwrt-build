e4b1020d72a6222fa862446771ef9ff20b54a32f
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-07-28 22:39:49 +0800

    Create diy.sh

---
+++ patch/imm-24.10/bw/diy.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+#其他网络设置
+
+uci commit
+
+sed -i '/modem/d' /etc/opkg/distfeeds.conf
+sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
+sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
+
+sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.2/targets/ramips/mt7621/kmods/6.6.93-1-730c62a2d59c0c43f493ba6554a36be8' /etc/opkg/distfeeds.conf
+
+sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+
+
+#cp /etc/my-clash /etc/openclash/core/clash_meta
+
+#uci set network.cfg030f15.macaddr="8C:DA:$(date +"%d:%H:%M:%S")"
+#uci set network.cfg060f15.macaddr="9C:DA:$(date +"%d:%H:%M:%S")"
+#uci commit network
+#uci set wireless.default_radio0.macaddr='random'
+#uci commit wireless
+
+uci commit
+
+#/etc/init.d/network restart
+
+exit 0
