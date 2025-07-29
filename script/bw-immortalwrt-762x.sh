5e395bca1045983560103c796e6094fea331216c
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-07-29 21:30:32 +0800

    Update bw-immortalwrt-762x.sh

--- script/bw-immortalwrt-762x.sh
+++ script/bw-immortalwrt-762x.sh
@@ -5,7 +5,7 @@ sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
 
-mv $GITHUB_WORKSPACE/patch/imm-24.10/bw/mac80211.uc package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+#mv $GITHUB_WORKSPACE/patch/imm-24.10/bw/mac80211.uc package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 mv $GITHUB_WORKSPACE/patch/imm-24.10/bw/diy.sh package/base-files/files/etc/uci-defaults/zz-diy.sh
 #mv $GITHUB_WORKSPACE/patch/imm-24.10/bw/rc.local package/base-files/files/etc/rc.local
 mv $GITHUB_WORKSPACE/patch/imm-24.10/bw/my-mac.sh package/base-files/files/etc/my-mac.sh
