a984d88d2bb5bba77d8ab35975c4c37767dda2db
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-07-31 16:19:21 +0800

    Update 199-mt762x.sh

--- patch/imm-24.10/199-mt762x.sh
+++ patch/imm-24.10/199-mt762x.sh
@@ -19,8 +19,8 @@ sed -i '$a src/gz kmods https://downloads.immortalwrt.org/releases/24.10.2/targe
 # wifi设置
 #uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
 #uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
-uci set wireless.default_radio0.ssid=OpenWrt-5G
-uci set wireless.default_radio1.ssid=OpenWrt-2.4G
+uci set wireless.default_radio1.ssid=OpenWrt-5G
+uci set wireless.default_radio0.ssid=OpenWrt-2.4G
 uci set wireless.default_radio0.encryption=psk2+ccmp
 uci set wireless.default_radio1.encryption=psk2+ccmp
 uci set wireless.default_radio0.key=password
