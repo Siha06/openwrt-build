12cf2606675fe9b7f9e2f8e5676ec0ec94696479
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-08-06 02:51:44 +0800

    Create padavanonly-7621-science.sh

---
+++ script/padavanonly-7621-science.sh
@@ -0,0 +1,64 @@
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+mv $GITHUB_WORKSPACE/patch/7621-237imm/zz-diy package/base-files/files/etc/uci-defaults/zz-diy
+# 补丁r6620
+mv $GITHUB_WORKSPACE/patch/7621-237imm/dts/mt7621_netgear_r6220.dts target/linux/ramips/dts/mt7621_netgear_r6220.dts
+mv $GITHUB_WORKSPACE/patch/7621-237imm/dts/mt7621_netgear_sercomm_ayx.dtsi target/linux/ramips/dts/mt7621_netgear_sercomm_ayx.dtsi
+mv $GITHUB_WORKSPACE/patch/7621-237imm/dts/02_network target/linux/ramips/mt7621/base-files/etc/board.d/02_network
+sed -i 's/wan/gmac1/g' target/linux/ramips/dts/mt7621_netgear_wndr3700-v5.dts
+
+if grep -q "openclash=y" "$GITHUB_WORKSPACE/$CONFIG_FILE"; then
+    git clone --depth 1 -b core https://github.com/vernesong/OpenClash.git  package/openclash-core
+    tar -zxf package/openclash-core/master/meta/clash-linux-mipsle-softfloat.tar.gz -C package/base-files/files/etc/
+    mv package/base-files/files/etc/clash package/base-files/files/etc/my-clash
+    rm -rf package/openclash-core
+fi
+
+rm -rf package/emortal/luci-app-mwan3helper-chinaroute
+rm -rf feeds/luci/themes/luci-theme-argonv3
+rm -rf feeds/luci/themes/luci-theme-argonv2
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/themes/luci-theme-argon-config
+
+rm -rf feeds/packages/lang/golang
+git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+#删除自带的老旧依赖，ssr-plus，passwall
+rm -rf feeds/packages/net/{chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust}
+rm -rf feeds/packages/net/{shadowsocksr-libev,simple-obfs,sing-box,tcping,trojan-plus,tuic-client,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin}
+rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ssr-plus,luci-app-mosdns,luci-app-openclash}
+
+git clone --depth 1 https://github.com/vernesong/OpenClash.git  package/openclash
+git clone --depth 1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+
+rm -rf feeds/packages/net/{v2ray-geodata,mosdns}
+git clone --depth 1 https://github.com/sbwml/luci-app-mosdns -b v5-lua package/mosdns
+git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+#rm -rf feeds/packages/multimedia/{UnblockNeteaseMusic-Go,UnblockNeteaseMusic}
+#rm -rf feeds/luci/applications/{luci-app-unblockneteasemusic,luci-app-unblockneteasemusic-go,luci-app-unblockmusic}
+#git clone --depth 1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git  package/luci-app-unblockneteasemusic
+#git clone --depth 1 https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
+
+#rm -rf feeds/packages/net/adguardhome
+#git clone --depth 1 https://github.com/kenzok8/small-package.git package/kz8-small
+#mv package/kz8-small/adguardhome package/adguardhome
+#mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
+#mv package/kz8-small/luci-app-ikoolproxy package/luci-app-ikoolproxy
+#mv package/kz8-small/luci-app-wrtbwmon package/luci-app-wrtbwmon
+#mv package/kz8-small/wrtbwmon package/wrtbwmon
+#rm -rf package/kz8-small
+
+#git clone --depth 1 https://github.com/coolsnowwolf/lede.git package/lede
+#mv package/lede/package/lean/luci-app-leigod-acc package/luci-app-leigod-acc
+#mv package/lede/package/lean/leigod-acc package/leigod-acc
+#rm -rf package/lede
+
+#git clone --depth 1 -b openwrt-21.02 https://github.com/immortalwrt/packages.git package/imm21-packages
+#mv package/imm21-packages/net/conntrack-tools package/conntrack-tools
+#rm -rf package/imm21-packages
