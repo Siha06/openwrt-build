9ab07d7313e7859d1cd300111a47fb1727e15ac4
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-08-22 18:15:54 +0800

    Update padavanonly-7621-ax.sh

--- script/padavanonly-7621-ax.sh
+++ script/padavanonly-7621-ax.sh
@@ -1,20 +1,20 @@
-sed -i 's/192.168.6.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.5.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/192.168.6.1/10.3.2.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/10.3.2.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 #sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-#mv $GITHUB_WORKSPACE/patch/7621-237imm/zz-diy package/base-files/files/etc/uci-defaults/zz-diy
+mv $GITHUB_WORKSPACE/patch/7621-237imm/zz-diy package/base-files/files/etc/uci-defaults/zz-diy
 
 #白雾定制
 #sed -i 's/ImmortalWrt/TikTok/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/XiaoYuan/g' package/base-files/files/bin/config_generate
-mv $GITHUB_WORKSPACE/patch/tiktok/7621/bw-diy.sh package/base-files/files/etc/uci-defaults/zz-diy.sh
+#sed -i 's/ImmortalWrt/XiaoYuan/g' package/base-files/files/bin/config_generate
+#mv $GITHUB_WORKSPACE/patch/tiktok/7621/bw-diy.sh package/base-files/files/etc/uci-defaults/zz-diy.sh
 #mv $GITHUB_WORKSPACE/patch/tiktok/7621/bw-index.htm package/base-files/files/etc/bw-index.htm
-mv $GITHUB_WORKSPACE/patch/tiktok/7621/bw-school-index.htm package/base-files/files/etc/bw-index.htm
-rm -rf feeds/luci/applications/luci-app-ua2f
+#mv $GITHUB_WORKSPACE/patch/tiktok/7621/bw-school-index.htm package/base-files/files/etc/bw-index.htm
+#rm -rf feeds/luci/applications/luci-app-ua2f
 #rm -rf feeds/packages/net/ua2f/*
-git clone --depth 1 https://github.com/lucikap/luci-app-ua2f.git package/luci-app-ua2f
+#git clone --depth 1 https://github.com/lucikap/luci-app-ua2f.git package/luci-app-ua2f
 #git clone --depth 1 https://github.com/Zxilly/UA2F.git package/ua2f
 #mv package/ua2f/openwrt/* feeds/packages/net/ua2f
-rm -rf package/ua2f
+#rm -rf package/ua2f
 
 if grep -q "openclash=y" "$GITHUB_WORKSPACE/$CONFIG_FILE"; then
     git clone --depth 1 -b core https://github.com/vernesong/OpenClash.git  package/openclash-core
@@ -36,7 +36,7 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
 rm -rf feeds/packages/lang/golang
-git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 #删除自带的老旧依赖，ssr-plus，passwall
 rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ssr-plus,luci-app-mosdns}
 
@@ -52,6 +52,7 @@ rm -rf package/openclash
 
 rm -rf feeds/packages/net/adguardhome
 git clone --depth 1 https://github.com/kenzok8/small-package.git package/kz8-small
+mv package/kz8-small/adguardhome package/adguardhome
 mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
 mv package/kz8-small/luci-app-ikoolproxy package/luci-app-ikoolproxy
 mv package/kz8-small/luci-app-wrtbwmon package/luci-app-wrtbwmon
