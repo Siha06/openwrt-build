5c60d1bf8f3d5bac5861a18164bfcf0ad4a4bc06
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-09-17 03:26:27 +0800

    Update 998-dogking-sglwifi.sh

--- patch/ipq-vikingyfy/dogking/998-dogking-sglwifi.sh
+++ patch/ipq-vikingyfy/dogking/998-dogking-sglwifi.sh
@@ -46,9 +46,9 @@ uci commit
 
 uci set dhcp.lan.start='20'
 uci set dhcp.lan.limit='70'
-uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G1
-uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
-uci set wireless.default_radio2.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G2
+uci set wireless.default_radio0.ssid=OpenWrt-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G1
+uci set wireless.default_radio1.ssid=OpenWrt-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
+uci set wireless.default_radio2.ssid=OpenWrt-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G2
 uci commit wireless
 uci commit
 
