597a59f58425c4ac6ed2f7bb339e59d9ff076fb7
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-09-17 04:21:10 +0800

    Update 998-dogking-sglwifi.sh

--- patch/ipq-vikingyfy/dogking/998-dogking-sglwifi.sh
+++ patch/ipq-vikingyfy/dogking/998-dogking-sglwifi.sh
@@ -32,24 +32,52 @@ EOF
 # 32–63 192.168.1.32/27   
 # 64–95 192.168.1.64/27   
 
+uci set wireless.default_radio0.ssid=$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G1
+uci set wireless.default_radio1.ssid=$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
+uci del wireless.default_radio2
+uci commit wireless
+
+uci set wireless.wifinet=wifi-iface
+uci set wireless.wifinet2.device="radio2"
+uci set wireless.wifinet2.mode='ap'
+uci set wireless.wifinet2.ssid="OpenWrt"
+uci set wireless.wifinet2.encryption='psk2+ccmp'
+uci set wireless.wifinet2.key="123456789"
+uci set wireless.wifinet2.ifname="5G2AP1"
+uci set wireless.wifinet2.network="openwrt"
+
+uci set network.openwrt=interface
+uci set network.openwrt.proto='static'
+uci set network.openwrt.device="5G2AP1"
+uci set network.openwrt.ipaddr="10.10.1.1"
+uci set network.openwrt.netmask='255.255.255.0'
+
+uci set dhcp.openwrt=dhcp
+uci set dhcp.openwrt.interface="openwrt"
+
+uci add firewall zone
+uci set firewall.@zone[-1].name="openwrt"
+uci set firewall.@zone[-1].input='ACCEPT'
+uci set firewall.@zone[-1].output='ACCEPT'
+uci set firewall.@zone[-1].forward='ACCEPT'
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src="openwrt"
+uci set firewall.@forwarding[-1].dest='wan'
 uci add firewall rule
-uci set firewall.@rule[-1].src='lan'
-uci add_list firewall.@rule[-1].src_ip='10.5.1.16/28'
-uci add_list firewall.@rule[-1].src_ip='10.5.1.32/27'
-uci add_list firewall.@rule[-1].src_ip='10.5.1.64/27'
-uci set firewall.@rule[-1].dest='wan'
 uci set firewall.@rule[-1].name="ban"
+uci set firewall.@rule[-1].src='openwrt'
+uci add_list firewall.@rule[-1].src_ip='10.10.1.16/28'
+uci add_list firewall.@rule[-1].src_ip='10.10.1.32/27'
+uci add_list firewall.@rule[-1].src_ip='10.10.1.64/27'
+uci set firewall.@rule[-1].dest='wan'
 uci add_list firewall.@rule[-1].proto='all'
 uci set firewall.@rule[-1].target='REJECT'
 # uci set firewall.@rule[-1].enabled='0'
 uci commit
 
-uci set dhcp.lan.start='20'
-uci set dhcp.lan.limit='70'
-uci set wireless.default_radio0.ssid=OpenWrt-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G1
-uci set wireless.default_radio1.ssid=OpenWrt-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
-uci set wireless.default_radio2.ssid=OpenWrt-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G2
-uci commit wireless
+uci set dhcp.openwrt.start='20'
+uci set dhcp.openwrt.limit='70'
+
 uci commit
 
 exit 0
