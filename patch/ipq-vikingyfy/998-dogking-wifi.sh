9a9017a9ad84f8f4d52dd82733cb828113a1d37d
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-09-16 16:41:54 +0800

    Create 998-dogking-wifi.sh

---
+++ patch/ipq-vikingyfy/998-dogking-wifi.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+sed -i '/passwall/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/nss/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/qualcommax/d' /etc/apk/repositories.d/distfeeds.list
+sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.12.44-1-a6f9a9cd3032604645b5df27b24dcf61/packages.adb' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/packages/packages.adb' /etc/apk/repositories.d/distfeeds.list
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+cp /etc/my-clash /etc/openclash/core/clash_meta
+#不用重启network，源码自带
+
+cat << EOF > /etc/config/ddnsto
+config ddnsto
+	option enabled '1'
+	option feat_port '3033'
+	option feat_enabled '0'
+	option index '1'
+	option token '18b8b805-1147-40dd-a4c5-94b05206138b'
+EOF
+/etc/init.d/ddnsto restart
+
+uci del wireless.default_radio0
+uci del wireless.default_radio1
+uci del wireless.default_radio2
+uci commit
+
+
+exit 0
