7be4b86df7c5a41413d7feedfbed50ff360b7f01
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-11-27 03:21:01 +0800

    Update x86-openwrt.sh

--- script/x86-openwrt.sh
+++ script/x86-openwrt.sh
@@ -3,9 +3,9 @@
 sed -i 's/192.168.1.1/192.168.86.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.86.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/${defaults ? 0 : 1}/0/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
-mv $GITHUB_WORKSPACE/patch/openwrt-24.10/199-x86.sh package/base-files/files/etc/uci-defaults/199-x86.sh
+mv $GITHUB_WORKSPACE/patch/openwrt-24.10/2-199-x86.sh package/base-files/files/etc/uci-defaults/199-x86.sh
 
-if grep -q "openclash=y" "$GITHUB_WORKSPACE/$CONFIG_FILE"; then
+
 if grep -q "openclash=y" "$GITHUB_WORKSPACE/$CONFIG_FILE"; then
     echo "✅ 已选择 luci-app-openclash，添加 openclash core"
     mkdir -p files/etc/openclash/core
