13f8ff8259003b8b2fe00b2c5d422c0757d3d6a0
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-11-27 03:19:59 +0800

    Update 2-199-x86.sh

--- patch/openwrt-24.10/2-199-x86.sh
+++ patch/openwrt-24.10/2-199-x86.sh
@@ -32,56 +32,28 @@ uci delete ttyd.@ttyd[0].interface
 # 设置所有网口可连接 SSH
 uci set dropbear.@dropbear[0].Interface=''
 
+uci del dhcp.lan.ra
+uci del dhcp.lan.ra_slaac
+uci del dhcp.lan.dns_service
+uci del dhcp.lan.ra_flags
+uci del network.globals.ula_prefix
+uci del dhcp.lan.dhcpv6
+uci del dhcp.lan.ndp
+uci del network.wan6
+uci del network.lan.ip6assign
 
-#uci set luci.main.mediaurlbase=/luci-static/infinityfreedom
-#uci commit luci
-
+uci commit dhcp
+uci commit network
 uci commit
 
+sed -i '/passwall/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.openwrt.org#mirrors.pku.edu.cn/openwrt#g' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/openwrt/releases/24.10.1/targets/x86/64/kmods/6.6.86-1-af351158cfb5febf5155a3aa53785982' /etc/opkg/customfeeds.conf
+sed -i '/targets/d' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz pkg https://mirrors.pku.edu.cn/openwrt/releases/24.10.4/targets/x86/64/packages' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/openwrt/releases/24.10.4/targets/x86/64/kmods/6.6.110-1-484466e2719a743506c36b4bb2103582' /etc/opkg/distfeeds.conf
 sed -i '$a #src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/x86_64/kiddin9' /etc/opkg/customfeeds.conf
 
-OPENCLASH_FILE="/etc/config/openclash"
-if [ -f "$OPENCLASH_FILE" ]; then
-    mv /etc/my-clash /etc/openclash/core/clash_meta
-fi
-
-
-# 根据网卡数量配置网络
-eth_count=0
-for iface in /sys/class/net/*; do
-  iface_name=$(basename "$iface")
-  # 检查是否为物理网卡（排除回环设备和无线设备）
-  if [ -e "$iface/device" ] && echo "$iface_name" | grep -Eq '^eth|^en'; then
-    eth_count=$((count + 1))
-  fi
-done
-# 统计eth接口数量，大于1个则将eth0设为wan其它网口设为lan，只有1个则设置成DHCP模式
-#丢弃eth_count=$(ls /sys/class/net | grep -c '^eth')
-if [ $eth_count -gt 1 ]; then
-    uci set network.lan.ipaddr='192.168.23.1'
-
-    uci del dhcp.lan.ra_slaac
-    uci del dhcp.lan.dhcpv6
-    uci del dhcp.lan.ra_flags
-    uci add_list dhcp.lan.ra_flags='none'
-    uci set dhcp.lan.dns_service='0'
-
-    uci del network.wan6
-    uci del network.globals.packet_steering
-    uci del network.globals.ula_prefix
-    uci set network.lan.ip6assign='64'
-    uci set network.lan.ip6ifaceid='eui64'
-
-    uci set network.wan.device='eth0'
-    uci del network.cfg030f15.ports
-    ls /sys/class/net | awk '/^eth[0-9]+$/ && $0 != "eth0" {print "uci add_list network.cfg030f15.ports="$0}' | sh   
-else
-    uci set network.lan.proto='dhcp'
-    uci set dhcp.lan.ignore='1'
-fi
 
 /etc/init.d/network restart
 exit 0
