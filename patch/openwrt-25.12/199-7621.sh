943366b9892d74353218279682592e43dbc7a1da
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-03-10 12:25:02 +0800

    Create 199-7621.sh

---
+++ patch/openwrt-25.12/199-7621.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+uci -q get system.@imm_init[0] > "/dev/null" || uci -q add system imm_init > "/dev/null"
+
+if ! uci -q get system.@imm_init[0].system_chn > "/dev/null"; then
+	uci -q batch <<-EOF
+		set system.@system[0].timezone="CST-8"
+		set system.@system[0].zonename="Asia/Shanghai"
+
+		delete system.ntp.server
+		add_list system.ntp.server="ntp.tencent.com"
+		add_list system.ntp.server="ntp1.aliyun.com"
+		add_list system.ntp.server="ntp.ntsc.ac.cn"
+		add_list system.ntp.server="cn.ntp.org.cn"
+
+		set system.@imm_init[0].system_chn="1"
+		commit system
+	EOF
+fi
+
+
+# 设置默认防火墙规则，方便虚拟机首次访问 WebUI
+#uci set firewall.@zone[1].input='ACCEPT'
+
+# 设置主机名映射，解决安卓原生 TV 无法联网的问题
+#uci add dhcp domain
+#uci set "dhcp.@domain[-1].name=time.android.com"
+#uci set "dhcp.@domain[-1].ip=203.107.6.88"
+
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+#其他网络设置
+#uci set network.lan.ip6assign='64'
+#uci set network.lan.ip6ifaceid='eui64'
+
+#uci commit network
+
+sed -i '/modem/d' /etc/opkg/distfeeds.conf
+sed -i '/Modem/d' /etc/opkg/distfeeds.conf
+sed -i '/passwall/d' /etc/opkg/distfeeds.conf
+sed -i '/helloworld/d' /etc/opkg/distfeeds.conf
+sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
+sed -i 's#downloads.openwrt.org#mirrors.pku.edu.cn/openwrt#g' /etc/opkg/distfeeds.conf
+sed -i '/targets/d' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/openwrt/releases/25.12.0/targets/ramips/mt7621/kmods/6.12.71-1-364f8debbcd4cddc1f038dea515bf8a5' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz mt7621pkg https://mirrors.pku.edu.cn/openwrt/releases/25.12.0/targets/ramips/mt7621/packages/' /etc/opkg/distfeeds.conf
+
+#uci set network.USB=interface
+#uci set network.USB.proto='dhcp'
+#uci set network.USB.device='usb0'
+#uci commit network
+uci commit
+
+/etc/init.d/network restart
+
+exit 0
