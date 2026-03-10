54c11e5afdf5b55fc12dfadbc65943fb660db5c4
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-03-10 12:28:30 +0800

    Update openwrt-25.12.sh

--- script/openwrt-25.12.sh
+++ script/openwrt-25.12.sh
@@ -3,7 +3,7 @@
 sed -i 's/192.168.1.1/10.3.2.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/10.3.2.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/disabled='"'"'\${defaults ? 0 : 1}'"'"'/disabled=0/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
-mv $GITHUB_WORKSPACE/patch/openwrt-24.10/zz-mt7621.sh package/base-files/files/etc/uci-defaults/zz-mt7621.sh
+mv $GITHUB_WORKSPACE/patch/openwrt-25.12/zz-mt7621.sh package/base-files/files/etc/uci-defaults/zz-mt7621.sh
 
 
 # mipsle-softfloat
