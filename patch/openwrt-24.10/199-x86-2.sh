3ffaea5f2124477a0ea692e6ba315f5a22eeb6f4
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-03-10 18:19:33 +0800

    Update 199-x86-2.sh

--- patch/openwrt-24.10/199-x86-2.sh
+++ patch/openwrt-24.10/199-x86-2.sh
@@ -32,26 +32,19 @@ uci delete ttyd.@ttyd[0].interface
 # 设置所有网口可连接 SSH
 uci set dropbear.@dropbear[0].Interface=''
 
-uci del dhcp.lan.ra
-uci del dhcp.lan.ra_slaac
-uci del dhcp.lan.dns_service
-uci del dhcp.lan.ra_flags
-uci del network.globals.ula_prefix
-uci del dhcp.lan.dhcpv6
-uci del dhcp.lan.ndp
-uci del network.wan6
-uci del network.lan.ip6assign
 
-uci commit dhcp
-uci commit network
+
 uci commit
 
+sed -i '/modem/d' /etc/opkg/distfeeds.conf
+sed -i '/Modem/d' /etc/opkg/distfeeds.conf
+sed -i '/helloworld/d' /etc/opkg/distfeeds.conf
 sed -i '/passwall/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.openwrt.org#mirrors.pku.edu.cn/openwrt#g' /etc/opkg/distfeeds.conf
 sed -i '/targets/d' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz pkg https://mirrors.pku.edu.cn/openwrt/releases/24.10.4/targets/x86/64/packages' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/openwrt/releases/24.10.4/targets/x86/64/kmods/6.6.110-1-484466e2719a743506c36b4bb2103582' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/openwrt/releases/24.10.5/targets/x86/64/kmods/6.6.119-1-484466e2719a743506c36b4bb2103582' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz others https://mirrors.pku.edu.cn/openwrt/releases/24.10.5/targets/x86/64/packages' /etc/opkg/distfeeds.conf
 sed -i '$a #src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/x86_64/kiddin9' /etc/opkg/customfeeds.conf
 
 
