f25d9e097b8c5f348e9bcd1ee114929ee4ab3deb
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-04-02 13:44:30 +0800

    Update 199-7620-mac.sh

--- patch/openwrt-24.10/199-7620-mac.sh
+++ patch/openwrt-24.10/199-7620-mac.sh
@@ -65,6 +65,9 @@ uci set wireless.default_radio0.macaddr='random'
 uci set wireless.default_radio1.macaddr='random'
 uci add network device
 uci set network.@device[-1].name='eth0.2'
+uci set network.@device[-1].type='8021q'
+uci set network.@device[-1].ifname='eth0'
+uci set network.@device[-1].vid='2'
 uci set network.@device[-1].macaddr='5e:28:7a:bb:98:68'
 chmod +x /etc/mac_ip-change.sh
 cat << 'EOF' > /etc/rc.local
