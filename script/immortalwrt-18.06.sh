3bb519ac06da12fe51caa463e1a86cba36f24f09
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-04-02 20:43:49 +0800

    Update immortalwrt-18.06.sh

--- script/immortalwrt-18.06.sh
+++ script/immortalwrt-18.06.sh
@@ -2,6 +2,7 @@ sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/10.1.1.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
 cp package/base-files/files/etc/banner package/emortal/default-settings/files/openwrt_banner
 mv $GITHUB_WORKSPACE/patch/imm-18.06/199-mt762x package/base-files/files/etc/uci-defaults/zz-diy
