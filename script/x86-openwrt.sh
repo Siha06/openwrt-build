184a06522e8bd1f3363779c8691f0ccfc79a768e
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-05-11 21:03:51 +0800

    Update x86-openwrt.sh

--- script/x86-openwrt.sh
+++ script/x86-openwrt.sh
@@ -93,7 +93,7 @@ mv package/mypkg/imm24-packages/net/nps package/nps
 rm -rf package/mypkg/imm24-packages
 
 rm -rf feeds/packages/net/adguardhome
-git clone --depth 1 https://github.com/kenzok8/jell.git package/kz8-small
+git clone --depth 1 https://github.com/kenzok8/small-package.git package/kz8-small
 mv package/kz8-small/luci-app-homeproxy package/luci-app-homeproxy
 mv package/kz8-small/adguardhome package/adguardhome
 mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
@@ -108,14 +108,6 @@ mv package/kz8-small/vlmcsd package/vlmcsd
 rm -rf package/kz8-small
 
 
-
-#有编译openwrt环境后，加入UA2F模块和RKP-IPID模块
-#git clone --depth 1 https://github.com/lucikap/luci-app-ua2f.git package/luci-app-ua2f
-#git clone --depth 1 https://github.com/Zxilly/UA2F.git package/ua2f
-#git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
-#git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
-#rm -rf feeds/packages/net/ua2f
-#rm -rf feeds/luci/applications/luci-app-ua2f
 
 #git clone --depth 1 https://github.com/SunBK201/UA3F.git package/UA3F
 #修复TailScale配置文件冲突
