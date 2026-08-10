ab880ca9be320cf224e584e9c19c063340344a80
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-08-10 17:06:53 +0800

    Update 199-7621.sh

--- patch/openwrt-24.10/199-7621.sh
+++ patch/openwrt-24.10/199-7621.sh
@@ -47,13 +47,12 @@ sed -i '/helloworld/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.openwrt.org#mirrors.pku.edu.cn/openwrt#g' /etc/opkg/distfeeds.conf
 sed -i '/targets/d' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/openwrt/releases/24.10.6/targets/ramips/mt7621/kmods/6.6.127-1-f31f6f85a36836e510d64a18a9a5f1bf' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz mt7621pkg https://mirrors.pku.edu.cn/openwrt/releases/24.10.6/targets/ramips/mt7621/packages/' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/openwrt/releases/24.10.8/targets/ramips/mt7621/kmods/6.6.144-1-f31f6f85a36836e510d64a18a9a5f1bf' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz mt7621pkg https://mirrors.pku.edu.cn/openwrt/releases/24.10.8/targets/ramips/mt7621/packages/' /etc/opkg/distfeeds.conf
 
-#uci set network.USB=interface
-#uci set network.USB.proto='dhcp'
-#uci set network.USB.device='usb0'
-#uci commit network
+uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')
+uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
+
 uci commit
 
 /etc/init.d/network restart
